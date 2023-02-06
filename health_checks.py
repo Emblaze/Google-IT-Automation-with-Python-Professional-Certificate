ca22cc96a9d89e8a685b6deb6f86d203b7e1ff4b
Emblaze <benjamin.masse@gmail.com>
2023-02-06 11:18:46 +0100

    Add Another Rebasing Example

--- health_checks.py
+++ health_checks.py
@@ -1,19 +1,53 @@
 #!/usr/bin/env python3
+
+import os
+import sys
 import shutil
 import psutil
+import socket
 
-def check_disk_usage(disk):
-  du = shutil.disk_usage(disk)
-  free = du.free / du.total * 100
-  return free > 20
+def check_reboot():
+    """"Returns True if the compurter has a pending reboot."""
+    return os.path.exists("/run/reboot-required")
+    
+def check_disk_full(disk, min_gb, min_percent):
+    """Returns True if there isn't enough disk space, False otherwise."""
+    du = shutil.disk_usage(disk)
+    # Calculate the percentage of free space        
+    percent_free = du.free / du.total * 100
+    # Calculate how many free gigabytes 
+    gigabytes_free = du.free / 2**30
+    if gigabytes_free < min_gb or percent_free < min_percent:
+        return True
+    return False
 
-def check_cpu_usage():
-  usage = psutil.cpu_percent(1)
-  return  usage < 70
+def check_root_full():
+    """Returns True if the root partition is full, False otherwise."""
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
+    
+def check_cpu_constrained():
+    """Returns True if the CPU is having too much usagge, False otherwise."""
+    return psutil.cpu_percent(1) > 75
 
-"""Main body of script that checks if the 2 conditions described in the 2 functions are false
-"""
-if not check_disk_usage("/") or not check_cpu_usage():
-  print("Error!")
-else:
-  print("Everything is OK!")
+def check_no_network():
+    """Returns True if it fails to resolve Google's URL, False otherwise."""
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
+        
+def main():
+    checks = [
+            (check_reboot, "Pending Reboot."),
+            (check_root_full, "Root partition full"),
+            (check_cpu_constrained, "CPU load too high."),
+            (check_no_network, "No working network.")
+        ]
+    everything_ok = True
+    for check, msg in checks:
+        if check():
+            print(msg)
+            everything_ok = False
+    if not everything_ok:
+        sys.exit(1)
